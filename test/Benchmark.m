55e71aaf98c73c056f7d3f77a477c349375a163e
Dmitry Kravtsov <idkravitz@gmail.com>
2013-01-26 14:20:33 +1100

    Use center of mass and fix criteria in Benchmark.m

--- test/Benchmark.m
+++ test/Benchmark.m
@@ -2,19 +2,17 @@
 addpath("../lib");
 
 function [B, C] = splitMatrix(A, p)
+    epsilon = 1e-8;
     criteria = p'*A - dot(p, p);
-    B = A(:, criteria <= 0 );
-    C = A(:, criteria > 0);
+    B = A(:, criteria <= epsilon);
+    C = A(:, criteria > -epsilon);
 endfunction
 
-benchmarkSizes = [3 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100];
+benchmarkSizes = 10:10:1000;
 
 function w = genInitialSplit(X, splitFunc)
-    do % TODO: find a better a way to force non empty decomposition
-        w = rand(1, columns(X));
-        w = w / norm(w);
-        [X1, X2] = splitFunc(X, X*w');
-    until (columns(X1) > 0 && columns(X2) > 0)
+    cols = columns(X);
+    w = ones(1, cols) / cols;
 endfunction
 
 printf("size,serial_time,parallel_time,native_time\n");
